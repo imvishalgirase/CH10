28b82fb6a9f3b3374bf8b099f09dac00b3ba7aea
imvishalgirase <137286597+imvishalgirase@users.noreply.github.com>
2023-06-30 05:36:46 -0700

    Add files via upload

---
+++ CH10PR7.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+    int i=2000,y;
+    clrscr();
+    while(i<3000)
+    {
+      if(i%4==0)
+      {
+       printf("%d\t",i);
+       }
+       i++;
+       }
+       getch();
+       }
